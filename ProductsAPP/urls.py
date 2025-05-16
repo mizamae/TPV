752cf7694ed74cdb3459e5c3efbc45ddec3d4133
mizamae <mizamae@gmail.com>
2025-05-16 09:15:45 +0200

    added manual quantity input

--- ProductsAPP/urls.py
+++ ProductsAPP/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     
     path("bill/add", views.add_bill , name="MaterialsAPP_create_bill"),
     path("bill/edit/<str:code>/<int:tab>", views.edit_bill , name="MaterialsAPP_edit_bill"),
+    path("bill/edit/<str:code>/<int:tab>/<int:billPos>", views.edit_bill , name="MaterialsAPP_edit_billPos"),
     path("bill/MaterialsAPP_assign_customer/<str:code>", views.assign_customer , name="MaterialsAPP_assign_customer"),
     path("bill/resume/<str:code>", views.resume_bill , name="MaterialsAPP_resume_bill"),
     path("bill/append/<str:code>", views.append_barcode_to_bill , name="MaterialsAPP_append_barcode_to_bill"),
@@ -16,6 +17,7 @@ urlpatterns = [
     path("bill/reduce/<int:id>", views.reduce_bill_position , name="MaterialsAPP_reduce_bill_position"),
     path("bill/close/<str:code>", views.close_bill , name="MaterialsAPP_close_bill"),
     path("bill/delete/<str:code>", views.delete_bill , name="MaterialsAPP_delete_bill"),
+    path("bill/billposition/setmultiplier/<int:id>", views.setMultiplier_billPosition , name="ProductsAPP_billPosition_setMultiplier"),
 
     path("MaterialsAPP_check_stocks/", views.check_stock , name="MaterialsAPP_check_stocks"),
     path("MaterialsAPP_check_products/", views.check_products , name="MaterialsAPP_check_products"),
